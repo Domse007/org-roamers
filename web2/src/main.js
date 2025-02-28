bd4546aa6601f9f5cef8120077b66163919144f9
Dominik Keller <dominikkeller55@gmail.com>
2025-02-28 11:01:35 +0100

    Added website.

---
+++ web2/src/main.js
@@ -0,0 +1,122 @@
+import { syntaxHighlight } from 'katex'
+import hljs from 'highlight.js';
+import Graph from "graphology";
+import forceAtlas2 from "graphology-layout-forceatlas2";
+// TODO:
+import FA2Layout from "graphology-layout-forceatlas2/worker";
+import Sigma from "sigma";
+
+const MIN_SIZE = 200;
+const BORDER_SIZE = 10;
+const panel = document.getElementById('org-preview-dragging-area');
+const panel_wrapper = document.getElementById('org-preview-wrapper');
+
+let m_pos;
+
+export function resize(e) {
+  const dx = m_pos - e.x;
+  m_pos = e.x;
+  const new_width = Math.max((parseInt(getComputedStyle(panel_wrapper, '').width) + dx), MIN_SIZE);
+  panel_wrapper.style.width = new_width + "px";
+}
+
+export function setupPreview() {
+  panel.addEventListener("mousedown", function(e){
+    if (e.offsetX < BORDER_SIZE) {
+      m_pos = e.x;
+      document.addEventListener("mousemove", resize, false);
+    }
+  }, false);
+
+  document.addEventListener("mouseup", function(){
+    document.removeEventListener("mousemove", resize, false);
+  }, false);
+}
+
+export const syntaxHighlightSite = () => {
+  Array.from(document.getElementsByClassName("src"))
+    .forEach((item) => {
+      hljs.highlightElement(item)
+    }); 
+}
+
+export const preview = (name) => {
+  fetch(`/org?title=${name}`)
+    .then((response) => {
+      console.log(response);
+      return response.text();
+    }).then((html) => {
+      console.log(html);
+      document.getElementById('org-preview').innerHTML = html;
+      renderMathInElement(document.getElementById('org-preview'));
+      syntaxHighlightSite();
+    });	  
+};
+
+// Create a graphology graph
+const graph = new Graph();
+
+const updateGraph = () => {
+  fetch(`/graph`)
+    .then((resp) => resp.json())
+    .then((text) => JSON.parse(text))
+    .then((json) => {
+      json["nodes"].forEach((node) => {
+	graph.addNode(node, { label: node, x: temp, y: 0, size: 10, color: "blue"});
+      })
+      setupGraph()
+    })
+}
+
+export function setupGraph() {
+  const positions = forceAtlas2(graph, {
+    iterations: 50,
+    settings: {
+      gravity: 10
+    }
+  });
+
+  forceAtlas2.assign(graph);
+
+  const sigmaInstance = new Sigma(graph, document.getElementById("graph"));
+}
+
+const search = async (query) => {
+  const resp = await fetch(`/search?q=${query}`);
+  const text = await resp.json();
+  const res = JSON.parse(text);
+  return res;
+}
+
+const searchInput = document.getElementById('search-input');
+let searchSuggestion = document.getElementById('search-suggestion-wrapper');
+
+const InputHandler = (event) => {
+  const query = searchInput.value;
+  searchSuggestion.innerHTML = "";
+  search(query).then((res) => {
+    res["results"].forEach((e) => {
+      searchSuggestion.innerHTML += `<div onclick="preview('${e.title}')" style="padding: 5px; cursor: pointer;" onmouseout="this.style.background='gray';" onmouseover="this.style.background='lightgray';">${e.title}</div>`;
+    })
+  })
+};
+
+export function setupSearchBarEventListener() {
+  searchInput = document.getElementById('search-input');
+  searchSuggestion = document.getElementById('search-suggestion-wrapper');
+  
+  searchInput.addEventListener("input", InputHandler);
+  searchInput.addEventListener("focus", InputHandler);
+
+  searchInput.addEventListener("blur", () => {
+    setTimeout(() => {
+      searchSuggestion.innerHTML = "";
+    }, 250);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  setupPreview()
+  preview("Kreise");
+  setupSearchBarEventListener();
+});
