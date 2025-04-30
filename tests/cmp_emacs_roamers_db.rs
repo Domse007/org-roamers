b321a5e99aae1e7d8dad8bcf52052bfca9adb40a
Dominik Keller <dominikkeller55@gmail.com>
2025-04-30 18:29:00 +0200

    Fix CI.

--- tests/cmp_emacs_roamers_db.rs
+++ tests/cmp_emacs_roamers_db.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf, str::FromStr};
+use std::{path::{Path, PathBuf}, str::FromStr};
 
 use org_roamers::sqlite::SqliteConnection;
 
@@ -45,7 +45,11 @@ fn test_node_table<const PARAMS: usize>(
 ) {
     for (emacs_node, roamers_node) in emacs_nodes.iter().zip(roamers_nodes) {
         assert_eq!(emacs_node[0], roamers_node[0], "Id does not match!");
-        assert_eq!(emacs_node[1], roamers_node[1], "File does not match!");
+        assert_eq!(
+            fix_paths(emacs_node[1].clone()),
+            fix_paths(roamers_node[1].clone()),
+            "File does not match!"
+        );
         assert_eq!(emacs_node[2], roamers_node[2], "Level does not match!");
         assert_eq!(emacs_node[3], roamers_node[3], "Pos does not match!");
         assert_eq!(emacs_node[4], roamers_node[4], "Todos does not match!");
@@ -57,4 +61,16 @@ fn test_node_table<const PARAMS: usize>(
         // assert_eq!(emacs_node[9], roamers_node[9], "Properties does not match!");
         assert_eq!(emacs_node[10], roamers_node[10], "Olp does not match!");
     }
+}
+
+fn fix_paths<P: AsRef<Path>>(path: P) -> String
+where
+    PathBuf: From<P>,
+{
+    PathBuf::from(path)
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string()
 }
