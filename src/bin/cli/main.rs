e9b776738fc47724a74802f41a1b19a75e1e7fbe
Dominik Keller <dominikkeller55@gmail.com>
2025-05-16 21:58:03 +0200

    feat: updated cli and added help message.

--- src/bin/cli/main.rs
+++ src/bin/cli/main.rs
@@ -1,6 +1,8 @@
 use std::io::{stdin, stdout, BufRead, BufReader, Write};
 use std::net::TcpStream;
 
+const PS: &'static str = "cli> ";
+
 fn main() {
     let mut tcp = match TcpStream::connect("localhost:12568") {
         Ok(tcp) => tcp,
@@ -9,8 +11,6 @@ fn main() {
             return;
         }
     };
-
-    const PS: &'static str = "cli> ";
 
     print!("{PS}");
 
@@ -21,7 +21,14 @@ fn main() {
             println!("ERROR :: Got error while reading line: {e:?}");
         }
 
-        let _ = tcp.write(line.unwrap().as_bytes());
+        let line = line.unwrap();
+
+        if line.starts_with(".") {
+            handle_internal(&line);
+            continue;
+        }
+
+        let _ = tcp.write(line.as_bytes());
         let _ = tcp.write("\n".as_bytes());
 
         for line in BufReader::new(tcp.try_clone().unwrap()).lines() {
@@ -35,4 +42,24 @@ fn main() {
         print!("{}", PS);
         let _ = stdout().flush();
     }
+}
+
+fn handle_internal(line: &str) {
+    match line.trim() {
+        ".help" => help(),
+        ".exit" => std::process::exit(0),
+        unknown => println!("{unknown} unsupported command."),
+    }
+    print!("{}", PS);
+    let _ = stdout().flush();
+}
+
+fn help() {
+    println!("Supported internal commands:");
+    println!("    .help    Show this message");
+    println!("    .exit    Disconnect and exit cli\n");
+    println!("Supported server commands:");
+    println!("    exit     Exit the server");
+    println!("    conf     Print the server configuration");
+    println!("    open     Open the website using xdg-open");
 }
