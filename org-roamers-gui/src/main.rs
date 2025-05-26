d9d31637eba9e0a19b940359724bfad8e5d09147
Dominik Keller <dominikkeller55@gmail.com>
2025-05-26 11:47:06 +0200

    feat: basic gui for org-roamers.

--- org-roamers-gui/src/main.rs
+++ org-roamers-gui/src/main.rs
@@ -1,3 +1,100 @@
+use std::process::Command;
+
+use eframe;
+use org_roamers::server::ServerRuntime;
+use rfd::FileDialog;
+
+mod start;
+
 fn main() {
-    println!("Hello, world!");
+    const NAME: &str = env!("CARGO_PKG_NAME");
+
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([480.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        NAME,
+        options,
+        Box::new(|_cc| Ok(Box::<OrgRoamersGUI>::default())),
+    )
+    .unwrap();
+}
+
+struct OrgRoamersGUI {
+    ip_addr: String,
+    port: String,
+    roam_path: String,
+    runtime: Option<ServerRuntime>,
+}
+
+impl Default for OrgRoamersGUI {
+    fn default() -> Self {
+        Self {
+            ip_addr: "localhost".to_string(),
+            port: "5000".to_string(),
+            roam_path: "".to_string(),
+            runtime: None,
+        }
+    }
+}
+
+impl OrgRoamersGUI {
+    pub fn url(&self) -> anyhow::Result<String> {
+        let port: usize = self.port.parse()?;
+        Ok(format!("{}:{}", self.ip_addr, port))
+    }
+
+    pub fn url_with_protocol(&self) -> anyhow::Result<String> {
+        let port: usize = self.port.parse()?;
+        Ok(format!("http://{}:{}", self.ip_addr, port))
+    }
+}
+
+impl eframe::App for OrgRoamersGUI {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                let ip_label = ui.label("IP:");
+                ui.text_edit_singleline(&mut self.ip_addr)
+                    .labelled_by(ip_label.id);
+            });
+            ui.horizontal(|ui| {
+                let port_label = ui.label("Port:");
+                ui.text_edit_singleline(&mut self.port)
+                    .labelled_by(port_label.id);
+            });
+            ui.horizontal(|ui| {
+                let path_label = ui.label("Path:");
+                ui.text_edit_singleline(&mut self.roam_path)
+                    .labelled_by(path_label.id);
+                if ui.button("Pick").clicked() {
+                    match FileDialog::new().pick_folder() {
+                        Some(dir) => self.roam_path = dir.to_string_lossy().to_string(),
+                        None => {}
+                    }
+                }
+            });
+            let button_label = if self.runtime.is_some() {
+                "Stop Server"
+            } else {
+                "Start Server"
+            };
+            ui.horizontal(|ui| {
+                if ui.button(button_label).clicked() {
+                    if self.runtime.is_some() {
+                        let rt = self.runtime.take();
+                        rt.unwrap().graceful_shutdown().unwrap();
+                    } else {
+                        self.runtime = Some(start::start_server(&self).unwrap());
+                    }
+                }
+                if ui.button("Open Website").clicked() {
+                    let _ = Command::new("xdg-open")
+                        .arg(self.url_with_protocol().unwrap())
+                        .status();
+                }
+            });
+        });
+    }
 }
---
+++ org-roamers-gui/src/start.rs
@@ -0,0 +1,69 @@
+use std::{fs, path::PathBuf};
+
+use org_roamers::{
+    ServerState, StaticServerConfiguration,
+    api::APICalls,
+    server::{self, ServerRuntime},
+};
+
+use crate::OrgRoamersGUI;
+
+#[cfg(not(target_os = "windows"))]
+pub fn config_path() -> PathBuf {
+    PathBuf::from("/etc/org-roamers/")
+}
+
+#[cfg(target_os = "windows")]
+pub fn config_path() -> PathBuf {
+    std::env::var("APPDATA").map(PathBuf::from).unwrap()
+}
+
+fn html_path() -> PathBuf {
+    let mut path = config_path();
+    path.push("html_settings.json");
+    path
+}
+
+fn server_conf_path() -> PathBuf {
+    let mut path = config_path();
+    path.push("server_conf.json");
+    if !path.exists() {
+        PathBuf::from("./server_conf.json")
+    } else {
+        path
+    }
+}
+
+pub fn start_server(ctx: &OrgRoamersGUI) -> anyhow::Result<ServerRuntime> {
+    let url = ctx.url()?;
+
+    let calls = APICalls {
+        default_route: server::default_route_content,
+        get_graph_data: server::get_graph_data,
+        get_org_as_html: server::get_org_as_html,
+        serve_search_results: server::search,
+        serve_latex_svg: server::get_latex_svg,
+        get_status_data: server::get_status_data,
+    };
+
+    let server_configuration = match fs::read_to_string(server_conf_path()) {
+        Ok(content) => serde_json::from_str(content.as_str()).unwrap(),
+        Err(err) => {
+            tracing::error!("Failed to load config: {err}");
+            StaticServerConfiguration::default()
+        }
+    };
+
+    let mut state = ServerState::new(
+        html_path(),
+        ctx.roam_path.clone().into(),
+        server_configuration,
+    )
+    .unwrap();
+
+    if let Err(err) = state.sqlite.insert_files(&ctx.roam_path) {
+        anyhow::bail!("An error occured: {err}");
+    }
+
+    Ok(server::start_server(url, calls, state).unwrap())
+}
